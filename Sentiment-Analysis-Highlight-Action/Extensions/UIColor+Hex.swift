8d3cda3034e603c57a3706d0ad463dd9816c76d5
Kyle Weiner <kyle@kylemade.com>
2016-02-17 20:32:39 -0500

    Implement highlight action

---
+++ Sentiment-Analysis-Highlight-Action/Extensions/UIColor+Hex.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Hex.swift
+//  Created by Kyle Weiner on 2/17/16.
+//
+
+import UIKit
+
+extension UIColor {
+    func hexString() -> String {
+        var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+
+        return String(format: "#%02X%02X%02X", Int(r * 255), Int(g * 255), Int(b * 255))
+    }
+}
